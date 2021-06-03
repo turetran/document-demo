22b40c7ffad796961e7174db4c3be8519c17b47a
Tue Tran <trtue2002@gmail.com>
2021-06-03 13:10:44 +0700

    debug

--- pages/api/updateshh.js
+++ pages/api/updateshh.js
@@ -6,11 +6,23 @@ export default async (req, res) => {
     let key = body.key
     let title = body.title
 
-    firebase.database().ref('/doc/shh/content').update({[key]: {content: body.content, title, key}}, error => {
-		//trigger
-        if (error) console.log("updateshh", error)
-        else console.log("updateshh", "success")
-    })
-    
-    return res.status(200).json({res:'OK'})
+    console.log("body", body)
+
+    const temp =  () => new Promise((resolve, reject) => {
+        firebase.database().ref('/doc/shh/content').update({ [key]: { content: body.content, title, key } }, error => {
+            resolve("update done!-" + error)
+        })
+    } )
+
+    /*
+    let result = then(() => {
+        firebase.database().ref('/doc/shh/content').update({ [key]: { content: body.content, title, key } }, error => {
+            return "update done!-" + error
+        })
+    })*/
+
+    let data = await temp()
+    console.log("data", data)
+
+    return res.status(200).json({ resm: data })
 }
