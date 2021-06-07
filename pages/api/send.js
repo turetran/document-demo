da707423a3b2fe522231f802afdd5dd8d0df682b
Tue Tran <trtue2002@gmail.com>
2021-06-07 13:41:51 +0700

    fix bug

--- pages/api/send.js
+++ pages/api/send.js
@@ -15,37 +15,59 @@ export default async (req, res) => {
     }
 
 
-    if (body.kind == "new") { // create new
-        // check not duplicate title
-        let node = (await firebase.database().ref("/doc/shh/titles").orderByChild("title").equalTo(body.title).once("value")).val()
-        if (node) {
-            return res.status(400).json({ result: 'Lỗi: Tựa đề bị trùng.' })
+    const pros = () => new Promise(async (resolve) => {
+        if (body.kind == "new") { // create new
+            // check not duplicate title
+            let node = (await firebase.database().ref("/doc/shh/titles").orderByChild("title").equalTo(body.title).once("value")).val()
+            if (node) {
+                return res.status(400).json({ result: 'Lỗi: Tựa đề bị trùng.' })
+            }
+
+            firebase.database().ref('/doc/shh/titles/order').once('value').then(data => {
+                let order = data.val()
+                let cTime = Date.now()
+                if (!order) return
+                body.order = order
+                body.createTime = cTime
+                body.key = (cTime - startTime).toString()
+                const pro1 = new Promise((res1) => {
+                    firebase.database().ref('/doc/shh/titles/' + body.key).set(body, error => res1("error1: " + error))
+                })
+                const pro2 = new Promise((res1) => {
+                    firebase.database().ref('/doc/shh/titles').update({ order: order + 1 }, error => res1("error1: " + error))
+                })
+                const pro3 = new Promise((res1) => {
+                    firebase.database().ref('/doc/shh/content/' + body.key).update({ key: body.key, content: "", title: body.title }, error => res1("error1: " + error))
+                })
+                Promise.all([pro1, pro2, pro3]).then(vals => {
+                    //console.log("vals", vals[0])
+                    resolve(vals)
+                })
+            })
+        } else { // update
+            firebase.database().ref('/doc/shh/titles/' + body.key).once('value').then(data => {
+                let _data = data.val()
+                let cTime = Date.now()
+                _data.modifiedTime = cTime
+                _data.order = body.order
+                _data.title = body.title
+                _data.descr = body.descr
+
+                const pro1 = new Promise((res1) => {
+                    firebase.database().ref('/doc/shh/titles/' + body.key).update(_data, error => res1("error1: " + error))
+                })
+                const pro2 = new Promise((res2) => {
+                    firebase.database().ref('/doc/shh/content/' + body.key).update({ title: body.title }, error => res2("error2: " + error))
+                })
+                Promise.all([pro1, pro2]).then(vals => {
+                    //console.log("vals", vals[0])
+                    resolve(vals)
+                })
+            })
         }
 
-        firebase.database().ref('/doc/shh/titles/order').once('value').then(data => {
-            let order = data.val()
-            let cTime = Date.now()
-            if (!order) return
-            body.order = order
-            body.createTime = cTime
-            body.key = (cTime - startTime).toString()
-            firebase.database().ref('/doc/shh/titles/' + body.key).set(body)
-            firebase.database().ref('/doc/shh/titles').update({ order: order + 1 })
-            firebase.database().ref('/doc/shh/content/' + body.key).update({ key: body.key, content: "", title: body.title })
-        })
-    } else { // update
-        firebase.database().ref('/doc/shh/titles/' + body.key).once('value').then(data => {
-            let _data = data.val()
-            let cTime = Date.now()
-            _data.modifiedTime = cTime
-            _data.order = body.order
-            _data.title = body.title
-            _data.descr = body.descr
+    })
+    let result = await pros()
 
-            firebase.database().ref('/doc/shh/titles/' + body.key).update(_data)
-            firebase.database().ref('/doc/shh/content/' + body.key).update({ title: body.title })
-        })
-    }
-
-    return res.status(200).json({ result: 'Thành công' })
+    return res.status(200).json({ result })
 }
